ea11d6aaaa40f6cc92ce41a7f253322c47415403
rjruiz-dev <rjruizsf@gmail.com>
2020-06-04 18:02:24 -0300

    creando tabla lenguages

---
+++ app/Http/Controllers/LenguageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lenguage;
+use Illuminate\Http\Request;
+
+class LenguageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Lenguage  $lenguage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lenguage $lenguage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Lenguage  $lenguage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lenguage $lenguage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Lenguage  $lenguage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lenguage $lenguage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Lenguage  $lenguage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lenguage $lenguage)
+    {
+        //
+    }
+}
---
+++ database/factories/LenguageFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Lenguage::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
