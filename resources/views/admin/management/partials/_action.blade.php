be4096a031c4f9d6503bcf246edd95fd22e35496
rjruiz-dev <rjruizsf@gmail.com>
2020-06-11 11:40:03 -0300

    Redireccion a dashboard.blade

---
+++ resources/views/admin/management/partials/_action.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $url_show }}" class="btn-show" title="Detalle: {{ $usuarios->name}}"><i class="fa fa-eye text-primary"></i></a> | 
+<a href="{{ $url_edit }}" class="modal-show edit" id="btn-btn-edit" title="Editar: {{ $usuarios->name}}"><i class="fa fa-edit text-success btn-btn-edit-user"></i></a> | 
+<a href="{{ $url_destroy }}" class="btn-delete" title="{{ $usuarios->name}}"><i class="fa fa-trash text-danger"></i></a>
+
