3948739e32688d51e39603a8057c0568553bd7cc
franco0310 <francocorvalan2@gmail.com>
2020-10-22 21:31:38 -0300

    cambios blade impresion

--- resources/views/admin/books/exportPDF.blade.php
+++ resources/views/admin/books/exportPDF.blade.php
@@ -62,20 +62,19 @@
 {{ $setting->library_phone}} 
 </pre>
             </td>
-            <td align="center">
+            <td align="center" style="width: 20%;">
                 <img class="logo" src="{{ public_path("/images/". $setting->logo) }}" width="64">    
             </td>
             <td align="right" style="width: 40%;">
-                <h3>Fecha: actual</h3> 
-<pre>
- 
-</pre>              
+                <div style="margin-top: -60px;">
+                    <h3>Fecha: {{Carbon\Carbon::now()->format('d-m-Y')}}</h3> 
+                </div>
             </td>
         </tr>
     </table>
 </div>
 <div class="info">
-    <table width="100%">
+    <table width="100%" style="color:white;     background: #999934;">
         <tr>
             <td align="left" style="width: 50%;">
                 <b>Siglas Autor: {{ $book->document->let_author }}&nbsp;&nbsp; Siglas Título: {{ $book->document->let_title }}&nbsp;&nbsp; </b>
@@ -90,10 +89,10 @@
     <table width="100%">
         <tr>
             <td align="left" style="width: 50%;">
-                <h2>Portada</h2>
+                <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Portada</i></h2>
             </td>
             <td align="right" style="width: 50%;">
-                <h2 align="left">Sobre el Documento:</h2>
+                <h2 align="left"> <i>Sobre el Documento</i></h2>
             </td>
         </tr>
     </table>
@@ -112,18 +111,34 @@
 <b>Subtipo de Documento: {{ $book->document->document_subtype->subtype_name  }} </b>      
 </pre>
             </td>            
-            <td align="right" style="width: 40%;   margin-bottom: 100px;">
+            <td align="right" style="width: 40%;">
+            
+            @if (( $book->second_author_id == NULL ) && ($book->third_author_id == NULL))   
+                          @php                       
+                          $texto = 'No tiene autores';
+                          @endphp
+                          @else
+                                @if (( $book->second_author_id != NULL ) && ($book->third_author_id != NULL))
+                                @php
+                                        $coma = ", ";
+                                @endphp
+                                @else
+                                @php
+                                        $coma = "";
+                            $texto = $book->second_author->creator_name." ".$coma." ".$book->third_author->creator_name;
+                                 @endphp
+                            @endif
             
                 <!-- <h2 align="left">Sobre el Documento</h2> -->
-                <div style="text-align: center;">
-                <b>La gran esperanza<br></b> 
-                <b> Rafael Serrano García</b><br>
-                Barcelon; Editorial Planeta SA; 1983; 2ª; 1 Vol.; 280; ISBN: 84-320-5683-9; &nbsp;
-                Disponible desde: 00-00-0000; Genero: Cuento Adecuado para: Todos;&nbsp; 
-                Idioma: Castellano;&nbsp; 
-                Valoración: 1<br> 
-                Versión 3.0. Interactivo<br>
-                S1-E1-Es3-L5<br>
+                <div style="text-align: center; margin-top: -150px;">
+                        <b>{{ $book->document->original_title != NULL ? $book->document->original_title : 'Sin titulo Original'}}<br></b> 
+                        <b> {{ $book->subtitle != NULL ? $book->subtitle : 'Sin Subtitulo'}}</b><br>
+                        {{ $texto }}; Editorial Planeta SA; 1983; 2ª; 1 Vol.; 280; ISBN: 84-320-5683-9; &nbsp;
+                        Disponible desde: 00-00-0000; Genero: Cuento Adecuado para: Todos;&nbsp; 
+                        Idioma: Castellano;&nbsp; 
+                        Valoración: 1<br> 
+                        Versión 3.0. Interactivo<br>
+                        S1-E1-Es3-L5<br>
                 </div>
 <!-- <pre>
 <p align="left"><b>Titulo: <i>{{ $book->document->let_author }}</i></b></p> 
