570a262dd012f79ddd352418b044550efe23c7e0
franco0310 <francocorvalan2@gmail.com>
2020-10-24 14:35:14 -0300

    config en web

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ Route::get('email', function () {
     return new App\Mail\AlertClaim(App\User::first());
 });
  
-Route::get('/config-cache', function() {      $exitCode = Artisan::call('config:cache');      return '<h1>Clear Config cleared</h1>';  });
+Route::get('/config-cache', function() {      $exitCode = Artisan::call('config:cache');      return '<h1>Config Cache</h1>';  });
+
+Route::get('/config-clear', function() {      $exitCode2 = Artisan::call('config:clear');      return '<h1>Config Clear</h1>';  });
+
 
 Route::get('/', 'HomeController@index')->name('index');
 Route::delete('cambiar{id}', 'HomeController@cambiar')->name('cambiar');
