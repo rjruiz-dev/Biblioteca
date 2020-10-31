9579fe89cb9b12cd534b0b0ebcbc2cc0a2508f6a
rjruiz-dev <rjruizsf@gmail.com>
2020-10-31 20:18:49 -0300

    cambio de color en datatable dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -123,4 +123,67 @@ class AdminController extends Controller
     {
         //
     }
+
+    public function dataTable()
+    {                    
+        $usuarios = User::with('statu')->where(function ($query) {
+            $query->where('status_id', '=', 3)
+                  ->orWhere('status_id', '=', 4);
+        })     
+        ->get();
+      
+        return dataTables::of($usuarios)
+            ->addColumn('membership', function ($usuarios){
+                if($usuarios->membership == null){
+                    return 'No tiene número de socio';
+                }else{
+                    return  '<i class="fa fa-checñ"></i>'.' '.$usuarios->membership."<br>";              
+                }
+            }) 
+          
+          
+            ->addColumn('user_photo', function ($usuarios){
+                $url=asset("/images/$usuarios->user_photo"); 
+                return '<img src='.$url.' border="0" width="40" class="img-rounded" align="center" />'; 
+            })
+            ->addColumn('name', function ($usuarios){                               
+                return
+                    '<i class="fa fa-user"></i>'.' '.$usuarios->name."<br>"; 
+                    '<i class="fa fa-user"></i>'.' '.$usuarios->nickname."<br>";             
+            }) 
+            ->addColumn('email', function ($usuarios){
+                return                    
+                    '<i class="fa fa-envelope"></i>'.' '.$usuarios->email;              
+            })             
+            // ->addColumn('status_id', function ($usuarios){
+
+            //     if($usuarios->statu['state_description'] == 'Inactivo'){    
+
+            //         return '<span class="label label-danger sm">'.$usuarios->statu['state_description'].'</span>';
+            //     }
+            //     if ($usuarios->statu['state_description'] == 'Pendiente'){
+
+            //         return '<span class="label label-warning sm">'.$usuarios->statu['state_description'].'</span>';
+
+            //     }else{
+
+            //         return '<span class="label label-success sm">'.$usuarios->statu['state_description'].'</span>';
+            //     }              
+            // })    
+            ->addColumn('created_at', function ($usuarios){
+                return $usuarios->created_at->format('d-m-y');
+            })                 
+            
+            ->addColumn('accion', function ($usuarios) {
+                return view('admin.users.partials._action', [
+                    'usuarios' => $usuarios,
+                    'url_show' => route('admin.users.show', $usuarios->id),                        
+                    'url_edit' => route('admin.users.edit', $usuarios->id),                              
+                    'url_destroy' => route('admin.users.destroy', $usuarios->id)
+                ]);
+            })           
+            ->addIndexColumn()   
+            ->rawColumns(['membership', 'user_photo', 'name', 'email', 'created_at', 'accion']) 
+            ->make(true);  
+    }
 }
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -104,4 +104,105 @@
         </div>
     </div>
 </div> 
+
+<div class="panel panel-primary" style="border-color: {{ $setting->skin }};"> 
+        <div class="panel-heading" style="background-color: {{ $setting->skin }};">
+            <h3 class="panel-title">Prestamos recientes</h3>
+        </div>
+        <div class="panel-body">
+            <table id="datatable" class="table table-hover" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>N° Usuario</th>                            
+                        <th>Perfil</th>       
+                        <th>Nombre</th>                                           
+                        <th>Email</th>   
+                        <th>Estado</th>  
+                        <th>Agregado</th>                                
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>                
+            </table>
+        </div>
+    </div> 
 @stop
+
+@push('styles')  
+    <link rel="stylesheet" href="/adminlte/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">   
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css">   
+@endpush
+
+@push('scripts')     
+    <script src="/adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="/adminlte/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script> 
+    <script src="https://cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>   
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
+    <script src="{{ asset('js/user.js') }}"></script>
+    
+    <script>
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    var fechaActual = day + '-' + month + '-' + year;
+
+        $('#datatable').DataTable({
+            responsive: true,
+            processing: true,
+            serverSide: true,
+            responsive: true,
+            columnDefs: [
+                { responsivePriority: 1, targets: 0 },
+                { responsivePriority: 10001, targets: 4 },
+                { responsivePriority: -1, targets: -1 }
+            ],
+            order: [ [0, 'desc'] ],           
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'copy',
+                    exportOptions: {
+                        stripHtml: false,
+                        columns: [0,1,2,3,4,5,6,7]
+                    }
+                },               
+                {
+                    extend: 'excel',
+                    title: 'informe-usuarios-'+ fechaActual,
+                    exportOptions: {                       
+                        columns: [0,1,2,4,5,6,7]
+                    }
+                },              
+                {
+                    extend: 'print',
+                    title: 'informe-usuarios-'+ fechaActual,
+                    exportOptions: {
+                        stripHtml: false,
+                        columns: [0,1,2,3,4,5,6,7]
+                    }
+                }
+                
+            ],             
+            ajax: "{{ route('users.table') }}",            
+            columns: [                
+                {data: 'id', name: 'id'},
+                {data: 'membership', name: 'membership'},    
+                {data: 'user_photo', name: 'user_photo'},
+                {data: 'name', name: 'name'},               
+                {data: 'email', name: 'email'}, 
+                {data: 'status_id', name: 'status_id'}, 
+                {data: 'created_at', name: 'agregado'},                  
+                {data: 'accion', name: 'accion'}                          
+            ]
+        });
+    </script>
+@endpush
