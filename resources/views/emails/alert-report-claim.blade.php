2e25635183e0f7e55ab9a42ee1ef11857680de8b
rjruiz-dev <rjruizsf@gmail.com>
2021-03-18 13:53:37 -0300

    escapar html de emial

--- resources/views/emails/alert-report-claim.blade.php
+++ resources/views/emails/alert-report-claim.blade.php
@@ -9,7 +9,7 @@ Fecha: {{ Carbon\Carbon::parse(Carbon\Carbon::now())->format('d-m-Y') }}
 Documentos: 
     @component('mail::table')
      | ID | Documento | Socio | Fecha Prestamo | Fecha Devolucion | Retraso |
-        |:----------|:------------|:------------|:------------|:------------|:------------|
+     |:----------|:------------|:------------|:------------|:------------|:------------|
     @foreach($prestamos as $prestamo) 
 
     | {{ $prestamo->id }} | {{ $prestamo->copy->document->title }} | {{ $prestamo->user->nickname }} | {{ Carbon\Carbon::parse($prestamo->date)->format('d-m-Y') }} | {{ Carbon\Carbon::parse($prestamo->date_until)->format('d-m-Y') }} | {{ Carbon\Carbon::parse($prestamo->date_until)->diffInDays(Carbon\Carbon::now()) }} |
