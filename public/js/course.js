92f5676727a308172634a24a5eea30f105b4980a
franco0310 <francocorvalan2@gmail.com>
2021-05-08 17:11:49 -0300

    guardar con enter

--- public/js/course.js
+++ public/js/course.js
@@ -6,20 +6,68 @@ $('body').on('click', '.modal-show', function(event) {
         title = me.attr('title');
 
     $('#modal-title').text(title);
-    $('#modal-btn-save').removeClass('hide')
-        .text(me.hasClass('edit') ? 'Actualizar' : 'Crear');
-
+    $('#modal-btn-save').removeClass('hide').text(me.hasClass('edit') ? 'Actualizar' : 'Crear');
+    
     $.ajax({
         url: url,
         dataType: 'html',
         success: function(response) {
             $('#modal-body').html(response);
+            // AAAAAAAAAAAAAAAAAAAAAAAA
+            $( "#course_name" ).focus();
+            // AAAAAAAAAAAAAAAAAAAAAAAA
         }
     });
 
     $('#modal').modal('show');
 });
 
+ // AAAAAAAAAAAAAAAAAAAAAAAA
+  $('#modal').keypress(function(e) {
+    if ($("#modal").hasClass('in') && (e.keycode == 13 || e.which == 13)) {
+        event.preventDefault();
+        // DE ACA PARA ABAJO HASTA LA MARCA. TENES QUE COPIAR TODA LA LOGICA DESDE EVENT.PREVENT..
+        var form = $('#modal-body form'),
+            url = form.attr('action'),
+            method = $('input[name=_method]').val() == undefined ? 'POST' : 'PUT';
+    
+        form.find('.help-block').remove();
+        form.find('.form-group').removeClass('has-error');
+    
+        $.ajax({
+            url: url,
+            method: method,
+            data: form.serialize(),
+            success: function(response) {
+                form.trigger('reset');
+                $('#modal').modal('hide');
+                $('#datatable').DataTable().ajax.reload(null, false);
+    
+                var swal_exito = response.swal_exito;
+                var swal_info_exito = response.swal_info_exito;
+    
+                swal({
+                    type: 'success',
+                    title: swal_exito,
+                    text: swal_info_exito
+                });
+            },
+            error: function(xhr) {
+                var res = xhr.responseJSON;
+                if ($.isEmptyObject(res) == false) {
+                    $.each(res.errors, function(key, value) {
+                        $('#' + key)
+                            .closest('.form-group')
+                            .addClass('has-error')
+                            .append('<span class="help-block"><strong>' + value + '</strong></span>');
+                    });
+                }
+            }
+        })
+        // FIN MARCA
+    }
+  });
+ // AAAAAAAAAAAAAAAAAAAAAAAA
 $('#modal-btn-save').click(function(event) {
     event.preventDefault();
 
